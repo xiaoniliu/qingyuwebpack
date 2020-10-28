7d8c08012263290e516cfd8329d1d3518d171a5c
jys2347 <18057950097@163.com>
2020-10-28 17:58:40 +0800

    add some webpack-loader

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-console.log('[process.env] ',process.env.NODE_ENV)
-console.log(2333)
+console.log('[process.env] ', process.env.NODE_ENV);
+console.log(2333);
+const arr = [1, 2, 3];
+console.log(...arr);
+const arr2 = arr.map((item) => item);
+console.log(arr2);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
-
+const SCRIPT = process.env.npm_lifecycle_event;
+const mode = SCRIPT === 'build' ? 'production' : 'development';
+const { log } = console;
+log(`run [script >>> ${SCRIPT}] [env >>> ${mode}]`);
 module.exports = {
   /**
    * webpack入口
@@ -10,19 +13,19 @@ module.exports = {
    *  entry1: 'xxx',
    *  entry2: 'xxx'
    * }
-   * 同时指定多个入口打包是会生成多个文件
+   * 同时指定多个入口打包时会生成多个文件
    */
   entry: `${__dirname}/src/index.js`,
   /**
    *  mode 模式
    *  指定process.env.NODE_ENV环境变量 也可以在package.json的scripts中指定--mode=xxx
    *  1. mode=development时会默认启用 NamedChunksPlugin 和 NamedModulesPlugin。
-   *  2. mode=production时会默认启用 FlagDependencyUsagePlugin, 
+   *  2. mode=production时会默认启用 FlagDependencyUsagePlugin,
    *  FlagIncludedChunksPlugin, ModuleConcatenationPlugin,
-   *  NoEmitOnErrorsPlugin, OccurrenceOrderPlugin, 
+   *  NoEmitOnErrorsPlugin, OccurrenceOrderPlugin,
    *  SideEffectsFlagPlugin 和 UglifyJsPlugin。
    */
-  mode: 'production',
+  mode,
   /**
    * 编译后文件输出的位置
    * [name] name占位符，如果指定了多个入口，可以通过name获取入口配置名称（上面的entry1和entry2）插入打包后的文件名，便于区分。
@@ -31,5 +34,62 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: `bundle.[hash].js`,
+  },
+  /**
+   * 定义一些loader
+   * 参考https://cloud.tencent.com/developer/section/1477522
+   * 以下user内的几种写法效果相同
+   */
+  module: {
+    /**
+     * 定义rules数组
+     */
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', { loader: 'css-loader', options: {} }],
+        include: path.resolve(__dirname, 'src'),
+      },
+      /**
+       * 如果使用了ejs(js 模板标签语法<%= xxx %>)语法，html-loader会与html-webpack-plugin冲突
+       */
+      {
+        test: /\.(html|htm)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            minimize: true,
+          },
+        },
+        include: path.resolve(__dirname, 'src'),
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: ['file-loader'],
+        include: path.resolve(__dirname, 'src'),
+      },
+      /**
+       * babel注意事项
+       * 以下的替换在使用 7.x (含7以上)
+       *babel-core 替换为 @babel/core
+       *|- 因为babel-core支持到 babel 6.x， 如果使用 6.x 以上则需要修改，而且不可以两个都安装，可能会出现冲突的问题。
+       *babel-preset-* 替换为 @babel/preset-*
+       *|- 比如使用 babel-preset-env 则需要换成 @babel/preset-env，在 .babelrc 配置文件中的 "presets": ["env"] 也需要修改为 "presets": ["@babel/preset-env"]。
+       *babel-plugin-* 替换为 @babel/plugin-*
+       *|- 比如使用 babel-plugin-transform-runtime 则需要替换为 @babel/plugin-transform-runtime； .babelrc配置文件也要相应的修改。
+       */
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        //排除掉node_module目录
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
 };
